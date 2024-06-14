9b8aad772df1700b284e29eb2888ae6431363cc6
Andrew Starr <ajstarr@gmail.com>
2024-06-15 10:41:11 +1200

    Rev B Mainboard and demo firmware

--- Firmware/v0.1/SG-2.cpp
+++ Firmware/v0.1/SG-2.cpp
@@ -7,30 +7,49 @@ using namespace daisy;
 using namespace daisysp;
 
 static DaisySeed hw;
-Fm2 voice1, voice2;
+Chorus chorus;
+static MoogLadder flt1, flt2;
+static VariableSawOscillator voice1, voice2;
 ReverbSc verb;
 Configuration config =
 {
-	{68, 73}	
+	{56, 61}	
 };
 float string_base_freq[2];
 bool env_hyst_flag[2];
+GPIO pitch_gate[2]; 
+
+float flt1_cutoff, flt2_cutoff;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	float string_env_1, string_env_2, pitch_1, pitch_2;
+	float sig1=0.f, sig2=0.f, string_env_1, string_env_2, pitch_1, pitch_2;
 
 	for (size_t i = 0; i < size; i++)
 	{
-		string_env_1 = GetEnv(ENV_1); 
-		string_env_2 = GetEnv(ENV_2); 
 		pitch_1 = GetPitch(PITCH_1);
+		if(pitch_1 != 0.f)
+		{
+			string_env_1 = GetEnv(ENV_1); 
+		    voice1.SetFreq(pitch_1);
+			flt1_cutoff = pitch_1 * FLT_TRACK_SLOPE + FLT_TRACK_OFFSET;
+			sig1 = voice1.Process() * string_env_1;
+			flt1.SetFreq(flt1_cutoff);
+			sig1 = flt1.Process(sig1);
+		}
+
 		pitch_2 = GetPitch(PITCH_2);
+		if(pitch_2 != 0.f)
+		{
+			string_env_2 = GetEnv(ENV_2); 
+		    voice2.SetFreq(pitch_2);
+			flt2_cutoff = pitch_2 * FLT_TRACK_SLOPE + FLT_TRACK_OFFSET;
+			sig2 = voice2.Process() * string_env_2;
+			flt2.SetFreq(flt2_cutoff);
+			sig2 = flt2.Process(sig2);
+		}
 
-	    if(pitch_1==0.f) voice1.Reset(); else voice1.SetFrequency(pitch_1);
-	    if(pitch_2==0.f) voice2.Reset(); else voice2.SetFrequency(pitch_2);
-
-		float sig = (voice1.Process() * string_env_1) + (voice2.Process() * string_env_2);
+		float sig = chorus.Process(sig1+sig2);
 //		out[0][i] = out[1][i] = sig;
 		verb.Process(sig, sig, &out[0][i], &out[1][i]);
 	}
@@ -48,6 +67,9 @@ int main(void)
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 	float samplerate = hw.AudioSampleRate();
 
+	pitch_gate[0].Init(seed::D13, GPIO::Mode::INPUT, GPIO::Pull::NOPULL);
+	pitch_gate[1].Init(seed::D14, GPIO::Mode::INPUT, GPIO::Pull::NOPULL);
+
     AdcChannelConfig adc_cfg[4];
     adc_cfg[0].InitSingle(seed::A0);
     adc_cfg[1].InitSingle(seed::A1);
@@ -55,18 +77,27 @@ int main(void)
     adc_cfg[3].InitSingle(seed::A3);
 	hw.adc.Init(adc_cfg, 4);
     hw.adc.Start();
+
+    chorus.Init(samplerate);
+    chorus.SetLfoFreq(.33f);
+    chorus.SetLfoDepth(1.f);
+    chorus.SetDelay(0.9f);
+	chorus.SetFeedback(0.3);
+
+    flt1.Init(samplerate);
+    flt1.SetRes(0.9);
+    flt2.Init(samplerate);
+    flt2.SetRes(0.9);
 	
     verb.Init(samplerate);
     verb.SetFeedback(0.9f);
-    verb.SetLpFreq(1000.0f);
+    verb.SetLpFreq(2000.0f);
 
 	voice1.Init(samplerate);
 	voice2.Init(samplerate);
-    voice1.SetIndex(0.3);
-    voice1.SetRatio(5);
-    voice2.SetIndex(0.3);
-    voice2.SetRatio(5);
-
+	voice1.SetWaveshape(0.5f);
+	voice2.SetWaveshape(0.5f);
+ 
 	hw.StartAudio(AudioCallback);
 	while(1) {}
 }
@@ -75,8 +106,12 @@ float GetPitch(int channel)
 {
 	float pos, pitch;
 
-	pos = hw.adc.GetFloat(channel);
-	if(pos < MIN_POS) pitch = 0.f; else pitch = string_base_freq[channel] * ((B3 * pow(pos, 3)) + (B2 * pow(pos, 2)) + (B1 * pos) + B0);
+	pitch = 0.f;
+	if(pitch_gate[channel].Read())
+	{
+		pos = hw.adc.GetFloat(channel);
+		pitch = string_base_freq[channel] * ((B3 * pow(pos, 3)) + (B2 * pow(pos, 2)) + (B1 * pos) + B0);
+	}
 	return pitch;
 }
 
--- Firmware/v0.1/SG-2.h
+++ Firmware/v0.1/SG-2.h
@@ -4,14 +4,14 @@
 #define ENV_2               3
 #define ENV_THR_1           0.010f
 #define ENV_THR_2           0.020f
-#define MIN_POS             0.01f  
-#define PITCH_COEFF_A       0.91040486
-#define PITCH_COEFF_B       2.64052850
-#define PITCH_GATE_DELAY    4800
 #define B0                  3.6835
 #define B1                  -6.9249
 #define B2                  6.9501
 #define B3                  -2.7343
+
+#define FLT_TRACK_SLOPE     8.f
+#define FLT_TRACK_OFFSET    200.f
+
 struct Configuration
 {
     int8_t  string_base_midi[2];
